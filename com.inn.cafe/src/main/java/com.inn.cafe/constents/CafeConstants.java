9abd310b29b63f969c453f8e9d38537f8a2962b2
TienManh <manhtienmai21112003@gmail.com>
2025-05-15 17:10:58 +0700

    Generate API of BILL (Xuất PDF)

--- com.inn.cafe/src/main/java/com.inn.cafe/constents/CafeConstants.java
+++ com.inn.cafe/src/main/java/com.inn.cafe/constents/CafeConstants.java
@@ -7,4 +7,7 @@ public class CafeConstants {
     public static final String INVALID_DATA = "Invalid Data.";
 
     public static final String UNAUTHORIZED_ACCESS = "Unauthorized access.";
+
+    public static final String STORE_LOCATION = "D:\\Stored-file\\Cafe-stored-file";
+
 }
